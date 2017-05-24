cdf8f79e8201467e2fdc974506e37b63ae814ea2
amosurya <amosurya@DIN56003323.corp.capgemini.com>
2017-05-24 12:13:10 +0530

    testing for file

--- GitDemo/src/main/java/com/java/GitDemoApplication.java
+++ GitDemo/src/main/java/com/java/GitDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hiiiiiiiiiiii");
 		SpringApplication.run(GitDemoApplication.class, args);
 		
 	}
